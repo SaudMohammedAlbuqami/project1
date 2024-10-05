c9008f41e1aa0bfb36a0bb1ec2167e6f20c4691e
Saud Mohammed Albuqami <saudm1817@gmail.com>
2024-10-05 04:44:06 +0300

    the new edit index.php
    Update index.php

--- index.php
+++ index.php
@@ -130,6 +130,45 @@ include './include/db_close.php';
 </div> 
 
 
+<script src="https://cdn.jsdelivr.net/npm/@tsparticles/confetti@3.0.3/tsparticles.confetti.bundle.min.js"></script>
+
+<script>
+  let winner = document.getElementById('winner');
+
+  winner.addEventListener('click',(event) =>{
+    const duration = 4 * 1000,
+  animationEnd = Date.now() + duration,
+  defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 0 };
+
+function randomInRange(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+const interval = setInterval(function() {
+  const timeLeft = animationEnd - Date.now();
+
+  if (timeLeft <= 0) {
+    return clearInterval(interval);
+  }
+
+  const particleCount = 50 * (timeLeft / duration);
+
+  // since particles fall down, start a bit higher than random
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 },
+    })
+  );
+  confetti(
+    Object.assign({}, defaults, {
+      particleCount,
+      origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 },
+    })
+  );
+}, 250);
+  })
+</script>
 
 
 <script src="./js/bootstrap.bundle.min.js"></script>
